f0b2a198166c748c1cf3ec24e0ae3b1f123e0732
Stiaan van Schalkwyk <stiaan.vanschalkwyk@sanlam.co.za>
2025-10-29 11:29:04 -0400

    final commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,7 +1,6 @@
 const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
-
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
@@ -24,50 +23,37 @@ public_users.post("/register", (req, res) => {
     return res.status(404).json({ message: "Unable to register user." });
 });
 
+async function fetchBooks() {
+    return books;
+}
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+public_users.get('/', async (req, res) => {
+    try {
+        const data = await fetchBooks();
+        res.send(JSON.stringify(data, null, 4));
+    } catch (error) {
+        return res.status(500).json({ message: "Oops!" });
+    }
 });
 
+async function getBook(isbn) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(books[isbn]);
+        }, 1000);
+    });
+}
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async (req, res) => {
     const isbn = req.params.isbn;
-    res.send(books[isbn]);
-});
-
-// Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author;
-    const results = searchBooks(author, "author");
-
-    if (Object.keys(results).length > 0) {
-        res.send(JSON.stringify(results, null, 4));
-    }
-    else {
-        return res.status(404).json({ message: "No books for this author" });
+    try {
+        const data = await getBook(isbn);
+        res.send(JSON.stringify(data, null, 4));
+    } catch (error) {
+        return res.status(500).json({ message: "Oops!" });
     }
 });
-
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title
-    const results = searchBooks(title, "title");
-
-    if (Object.keys(results).length > 0) {
-        res.send(JSON.stringify(results, null, 4));
-    }
-    else {
-        return res.status(404).json({ message: "No matching titles found." });
-    }
-});
-
-//  Get book review
-public_users.get('/review/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn]["reviews"]));
-});
-
-function searchBooks(text, attribute) {
+async function searchBooks(text, attribute) {
     let results = {};
     const needle = text.toUpperCase();
     for (const [key, value] of Object.entries(books)) {
@@ -77,6 +63,46 @@ function searchBooks(text, attribute) {
     }
     return results;
 }
+// Get book details based on author
+public_users.get('/author/:author', async (req, res) => {
+    const author = req.params.author;
+
+    try {
+        const data = await searchBooks(author, "author");
+        if (Object.keys(data).length > 0) {
+            res.send(JSON.stringify(data, null, 4));
+        }
+        else {
+            return res.status(404).json({ message: "No books for this author" });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "Oops!" });
+    }
+
+});
+
+// Get all books based on title
+public_users.get('/title/:title', async (req, res) => {
+    const title = req.params.title;
+
+    try {
+        const data = await searchBooks(title, "title");
+        if (Object.keys(data).length > 0) {
+            res.send(JSON.stringify(data, null, 4));
+        }
+        else {
+            return res.status(404).json({ message: "No matching titles found." });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: "Oops!" });
+    }
+});
+
+//  Get book review
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(JSON.stringify(books[isbn]["reviews"]));
+});
 
 
 module.exports.general = public_users;
